08e1496ce69f5fc027ed92c92037da511bebb4d2
uptonking <jinyaoo86@gmail.com>
2016-04-28 01:32:39 +0800

    init code

---
+++ TESTWCF/User.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TESTWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "User" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select User.svc or User.svc.cs at the Solution Explorer and start debugging.
+    public class User : IUser
+    {
+        public string ShowName(string name)
+        {
+            string wcfName = string.Format("调用WCF服务输出的文字为：{0}", name);
+            return wcfName;
+        }
+    }
+}
